4e9289cbcf8ab5dcb05c55bd85446f60f0470af3
Tanuj Gupta <103828630+kira-tanuj@users.noreply.github.com>
2022-11-28 16:02:42 +0530

    Update bank transfer.sol

--- bank transfer.sol
+++ bank transfer.sol
@@ -1,51 +1,27 @@
-pragma solidity ^0.5.11;
-contract PropertyTransferApp {
-    address public contractOwner;
-    constructor() public {
-        contractOwner = msg.sender;
-    }
-    modifier onlyOwner() {
-        require(msg.sender == contractOwner);
-        _;
-    }
-    struct Property {
-        uint256 id;
-        string name;
-        string owner;
-        uint256 value;
-        uint256 area;
-    }
-    mapping(uint256 => Property) public properties;
-    function addProperty(
-        uint256 _propertyid,
-        string memory _name,
-        string memory _owner,
-        uint256 _value,
-        uint256 _area
-    ) public onlyOwner {
-        properties[_propertyid].name = _name;
-        properties[_propertyid].owner = _owner;
-        properties[_propertyid].value = _value;
-        properties[_propertyid].area = _area;
-    }
-    function queryPropertyById(uint256 _propertyid)
-        public view returns (
-            string memory name,
-            string memory owner,
-            uint256 area,
-            uint256 value
-        )
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.7;
+
+contract Bank
+{
+    int bal;
+
+    constructor ()  
     {
-        return (
-            properties[_propertyid].name,
-            properties[_propertyid].owner,
-            properties[_propertyid].area,
-            properties[_propertyid].value
-        );
+        bal = 1;
     }
-    function transferPropertyOwnership(
-        uint256 _propertyid,
-        string memory _newOwner) public {
-        properties[_propertyid].owner = _newOwner;
+
+    function getBalance() view public returns(int)
+    {
+        return bal;
+    
+    }
+
+    function withdraw(int amt) public
+    {
+         bal = bal - amt;
+    }
+    function deposit(int amt) public
+    {
+        bal = bal + amt;
     }
 }
